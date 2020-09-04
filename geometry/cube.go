05cc3749251d83b663ac928cdc5529d2d444f27d
dung997bn <phaichisophan@gmail.com>
2020-09-04 21:10:10 +0700

    zero adge is not allowed

--- geometry/cube.go
+++ geometry/cube.go
@@ -1,6 +1,14 @@
 package geometry
 
+import (
+	"errors"
+)
+
 //CubeVolumne func
-func CubeVolumne(n int) int {
-	return n * n * n
+func CubeVolumne(n int) (int, error) {
+	if n != 0 {
+		return n * n * n, nil
+	}
+	return 0, errors.New("Zero length edge is not allowed")
+
 }
